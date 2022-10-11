9f46f019668d9d87179bbe37b201430eaa9dfaf1
EmmanuelU <emmanuelurias60@icloud.com>
2022-10-10 18:20:03 -0700

    finished assignment

---
+++ vehicle.js
@@ -0,0 +1,76 @@
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+        this.fuel = 100;
+    }
+
+    accelerate() {
+        if (this.started) {
+            if(this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            }
+            else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        }
+        else {
+            alert("Start car")
+        }
+    }
+
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                }
+                else {
+                    console.log(this + 'has stopped');
+                    this.fuel = this.fuel - 1;
+                }
+            }
+            else {
+                console.log('out of fuel');
+                this.stop();
+            }
+        }
+        else {
+            alert('Start car');
+        }
+    }
+   
+    stop() {
+        console.log('engine off')
+        this.started = false;
+    }
+
+    typeOfVehicle() {
+        if(this.numberOfWheels == 8) {
+            console.log(this.model + " " + this.make + " is a Truck");
+        }
+        else if (this.numberOfWheels == 4) {
+            console.log(this.model + " " + this.make + " is a Car");
+        }
+        else if (this.numberOfWheels == 2) {
+            console.log(this.model + " " + this.make + " is a Bike");
+        }
+        else {
+            console.log('unkown type of vehicle');
+        }
+    }
+}
+
+module.exports = {
+    Vehicle
+}
